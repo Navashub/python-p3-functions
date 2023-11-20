b08291e8d23c1a100ee80446db0040d6976b18a5
Navashub <navasmuller01@gmail.com>
2023-11-20 12:39:12 +0300

    assignment done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,39 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+sum_result = add(10, 12)
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    return number / 2
+
+result = halve(8)
+print(result)
+
+results = halve("four")
+print(results)
+
+greet_programmer()
+
+greet("Naureen")
+
+greet_with_default()
+
+greet_with_default("Sunny")
+
+print(sum_result)
+
+
+
+
